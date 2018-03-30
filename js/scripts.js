5448889177ba43110cc65dcd7fb63f38f725ead9
Joy Paglinawan <virliejoyp@yahoo.com>
2018-03-30 14:02:50 -0700

    Work on business logic in jsscript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
-  $("#form").submit(function(event));
+$(function(){
+  $("#form").submit(function(event){
     event.preventDefault();
 
-    var input=$("#input");
-}
+    var input=$("#input").val()
+    $("#result").text(input)
+    
+    if(isNaN(parseInt(input))){
+      alert("Your mother is a hamster and your father smelt of elderberries!")
+
+    } else {
+      if ((parseInt(input)/3) % 1 === 0 && parseInt(input) !== 1) {
+        $("#result").text("I'm sorry, Dave. I'm afraid I can't do that.")
+
+      } else {
+
+        var numArray = input.split("")
+
+          for (var i=0; i < numArray.length; i++){
+            if(numArray[i] == 0){
+              $("#result").text("Beep!")
+              break
+            }else if (numArray[i] == 1){
+              $("#result").text("Boop!")
+            }
+          }
+        }
+      }
+    })
+  })
