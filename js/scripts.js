83c2f52e5622274ef2fd843006096c23a05a9119
Joy Paglinawan <virliejoyp@yahoo.com>
2018-04-04 13:18:13 -0700

    After much needed rest, realize that the project from last friday I've been working on since last night was being saved in the wrong directory. So far, start working on project according to a very well explained feedback.

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,68 @@
-$(function(){
-  $("#form").submit(function(event){
-    event.preventDefault();
+var array = [];
 
-    var input=$("#input").val()
-    $("#result").text(input)
-
-    numCheck(input)
-
-  });
-
-  //business logic//
-  // //Business logic here
-  // var beeper = function(input) {
-  //   var output = [];
-  //   for(var index = 0; index <= input; index ++){
-  //     if (index % 3 == 0 && index !== 0) {
-  //       output.push("I'm sorry, Dave. I'm afraid I can't do that.");
-  //     } else if (index.toString().includes("1")) {
-  //       output.push("Boop!");
-  //     } else if (index.toString().includes("0")) {
-  //       output.push("Beep!");
-  //     } else {
-  //       output.push(index);
-  //     }
-  //   }
-  //   return output;
+function numCheck(inputNumber){
+ var num = i.split()
+ var input = parseInt(inputNumber);
+ for (var i = 0; i <= input; i++){
+   if (i === 0) {
+     array.push("Beep!")
+   } else if (i === 1) {
+     array.push("Boop!")
+   } else if ((parseInt(inputNumber)/3) % 1 === 0) {
+     array.push("I'm sorry, Dave. I'm afraid I can't do that")
+   }else if(num [0] === 1){
+      array.push ("Boop!")
+    }else if ((parseInt(inputNumber)/3) % 1 === 0; num [0]=== 1)
 
 
 
 
 
-  function numCheck(input) {
-    var numArray = input.split("")
-    for (var i=0; i < numArray.length; i++){
-      if(numArray[i] == 0){
-        // $("#result").text("Beep!")
-        // alert("Beep!")
-        // break
-      }else  if (numArray[i] == 1){
-      // $("#result").text("Boop!")
-      // alert("Boop!")
-      // break
-    }else if ((parseInt(input)/3) % 1 === 0 && parseInt(input) !== 0){
-      // $("#result").text("I'm sorry, Dave. I'm afraid I can't do that.")
-      // alert("I'm sorry, Dave. I'm afraid I can't do that.")
-    }if(isNaN(parseInt(input))){
-        alert("Your mother is a hamster and your father smelt of elderberries!")
-        break
-      }
-    }
+  } else {
+    array.push(i)
   }
-});
+ }
+}
+
+
+// $(function(){
+//   $("#form").submit(function(event){
+//     event.preventDefault();
+//
+//     var input=$("#input").val()
+//     $("#result").text(input)
+//
+//     numCheck(input)
+//
+//   });
+//
+//   function numCheck(input) {
+//     var numArray = input.split("")
+//     for (var i=0; i < numArray.length; i++){
+//       if(numArray[i] == 0){
+//         $("#result").text("Beep!")
+//         alert("Beep!")
+//         break
+//       }else  if (numArray[i] == 1){
+//         $("#result").text("Boop!")
+//         alert("Boop!")
+//         break
+//       }else if ((parseInt(input)/3) % 1 === 0 && parseInt(input) !== 0){
+//         $("#result").text("I'm sorry, Dave. I'm afraid I can't do that.")
+//         alert("I'm sorry, Dave. I'm afraid I can't do that.")
+//       }if(isNaN(parseInt(input))){
+//         alert("Your mother is a hamster and your father smelt of elderberries!")
+//         break
+//       }
+//     }
+//   }
+// });
+
+
+
+
+
+
 
 
 
